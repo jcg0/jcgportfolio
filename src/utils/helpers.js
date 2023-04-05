0081035effaa4c786294e701974b347a5d6188aa
jcg0 <jg.awsprod@gmail.com>
2023-04-04 21:15:14 -0400

    added helper functions for validating email and name

---
+++ src/utils/helpers.js
@@ -0,0 +1,10 @@
+export function validateName(name) {
+  const nameReg = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+  return nameReg.test(String(name).toLowerCase());
+}
+
+export function validateEmail(email) {
+  const emailReg =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailReg.test(String(email).toLowerCase());
+}
